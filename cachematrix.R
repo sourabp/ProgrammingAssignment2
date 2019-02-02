a6f74015b1f362918f1b0957d4593d1be161ed52
sourab <sourab.pareek@innovaccer.com>
2019-02-02 15:13:36 +0530

    solved assigment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Takes a matrix as input and returns a list of functions to helper functions to
+## 1. Set the value of matrix
+## 2. Get the matrix
+## 3. Set the inverse of the matrix
+## 4. Get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv<-NULL
+  set<-function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get<-function() x
+  setInv <- function(inverse) inv<<-inverse
+  getInv <- function() inv
+  list(set=set, get=get,
+       setInv=setInv, getInv=getInv)
 
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv<-x$getInv()
+  if(!is.null(inv)){
+    inv
+  } else {
+    data<-x$get()
+    inv_val<-solve(data)
+    x$setInv(inv_val)
+    inv_val
+  }
 }
